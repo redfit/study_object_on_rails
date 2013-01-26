cd99923f5ed1cf090a926df2362c51a62b0b687f
daisuke fuchiwaki <daisuke.fuchiwaki@excel.co.jp>
2013-01-26 23:46:54 +0900

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloog::Application.config.secret_token = '905deb117e7137ea1a30339b251c8a165a318f80cc6b00860714beaf8a411fe782d4ec3d8a264d1eca188425654356b0aa58f15c079a036117ef1d43d6d2b335'
